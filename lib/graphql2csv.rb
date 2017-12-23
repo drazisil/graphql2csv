e6b269ca6403323be9e4a4d07b8dce43745d1f3e
Joseph W Becher <jwbecher@drazisil.com>
2017-12-23 13:14:25 -0500

    Moved query and config to seperate files

--- lib/graphql2csv.rb
+++ lib/graphql2csv.rb
@@ -1,42 +1,67 @@
+#!/usr/bin/env ruby
+
 require 'net/http'
 require 'uri'
 require 'json'
 require 'pp'
 
-class GraphQL2CSV
+def read_config
   
+  if not File.readable?("config.json")
+    return nil
+  end
+
+  return JSON.parse(File.read("config.json"))
 end
 
-if not ENV.has_key?('TOKEN')
+def read_query
+  
+  if not File.readable?("query.gql")
+    return nil
+  end
+
+  return File.read("query.gql").to_s
+end
+
+class GraphQL2CSV
+
+  config = read_config()
+  queryBody = read_query()
+
+
+  config['token'] = ENV['TOKEN'] if ENV.has_key?('TOKEN')
+
+  if config.nil? and not ENV.has_key?('TOKEN')
     puts "Please set the env: TOKEN"
     exit(1)
+  end
+
+  query = { 
+      query: queryBody
+  }.to_json
+
+  res = Net::HTTP.post URI('https://api.github.com/graphql'),
+                query,
+                "Content-Type" => "application/json",
+                "Authorization" => "bearer " + config['token']
+
+                # Check if not authorized
+                if res.is_a?(Net::HTTPUnauthorized)                
+                  puts "ERROR: Invaild token, please check."
+                  exit(1)
+                end
+
+                responseJSON = JSON.parse(res.body)
+
+                # Check if there was an error
+                if responseJSON['errors']
+                  puts ("Something went wrong fetching JSON, returned: " + responseJSON['errors'].join)
+                  exit(1)
+                end
+
+          data = responseJSON['data']
+
+                
+  puts JSON.pretty_generate(data) 
+  
 end
-
-query = { 
-    "query": "query {
-        user(login: \"drazisil\") {
-        createdAt
-        repositories(first: 100) {
-          nodes {
-            name
-            url
-            createdAt
-          }
-        }
-      }
-    }"
-}.to_json
-
-res = Net::HTTP.post URI('https://api.github.com/graphql'),
-               query,
-               "Content-Type" => "application/json",
-               "Authorization" => "bearer " + ENV['TOKEN']
-
-               puts ("Something went wrong fetching JSON, returned: " + res) if not res.is_a?(Net::HTTPSuccess)
-
-        responseJSON = JSON.parse(res.body)['data']
-
-        puts res.body
-
-               
-puts JSON.pretty_generate(responseJSON) 
